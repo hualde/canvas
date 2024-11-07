bbf5eb999781f90e49fc6f23ebf9dbdf7e2a24e7
hualde <javhualde@gmail.com>
2024-11-07 17:21:52 +0100

    altura commigc

--- src/pages/Canvas.tsx
+++ src/pages/Canvas.tsx
@@ -198,7 +198,7 @@ export function Canvas() {
           onChange={(e) => setComments(e.target.value)}
           onBlur={handleUpdateCanvasInfo}
           className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          rows={2}
+          rows={4}
           placeholder="Comments"
         />
       </div>
