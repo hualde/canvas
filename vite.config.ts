faba4aca50bf98e45e1490a72143040b762b46b4
hualde <javhualde@gmail.com>
2024-11-13 19:11:48 +0100

    lanfing003

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
+import path from 'path'
 
-// Carga las variables de entorno de .env.local
+// Load environment variables from .env.local
 dotenv.config({ path: '.env.local' })
 
 export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': process.env
-  }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
