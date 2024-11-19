1d9afe218b5b369d2ae3fd721f58607a21e4f244
hualde <javhualde@gmail.com>
2024-11-19 11:38:21 +0100

    pixelotro

--- src/pages/RegistrationCallback.tsx
+++ src/pages/RegistrationCallback.tsx
@@ -2,39 +2,42 @@ import React, { useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useNavigate } from 'react-router-dom';
 
+declare global {
+  interface Window {
+    twq?: (command: string, event: string, parameters?: object) => void;
+  }
+}
+
 export default function RegistrationCallback() {
-  const { isAuthenticated, isLoading } = useAuth0();
+  const { isAuthenticated, isLoading, user } = useAuth0();
   const navigate = useNavigate();
 
   useEffect(() => {
     console.log('RegistrationCallback: useEffect ejecutado');
     if (!isLoading) {
       if (isAuthenticated) {
-        // El usuario se ha registrado exitosamente
-        // Aquí es donde implementaremos el seguimiento de eventos
         console.log('RegistrationCallback: Usuario autenticado, llamando a trackRegistrationEvent');
         trackRegistrationEvent();
       }
-      // Redirigir al usuario a la página principal o al dashboard
       console.log('RegistrationCallback: Redirigiendo al dashboard');
       navigate('/dashboard');
     }
-  }, [isAuthenticated, isLoading, navigate]);
+  }, [isAuthenticated, isLoading, navigate, user]);
 
   function trackRegistrationEvent() {
     console.log('RegistrationCallback: Ejecutando trackRegistrationEvent');
-    // Aquí implementaremos el código de seguimiento de X Ads
     if (window.twq) {
       console.log('RegistrationCallback: Enviando evento a X Ads');
-      window.twq('event', 'tw-xxxxxxxx-registration', {
-        // Puedes añadir propiedades adicionales si es necesario
+      window.twq('event', 'tw-or5as-or5au', {
+        status: 'completed', // Asumimos que el registro se ha completado en este punto
+        conversion_id: user?.sub, // Usamos el ID único del usuario de Auth0
+        email_address: user?.email // Usamos el email del usuario de Auth0
       });
     } else {
       console.log('RegistrationCallback: window.twq no está disponible');
     }
   }
 
-  // Muestra un loader mientras se procesa la autenticación
   if (isLoading) {
     console.log('RegistrationCallback: Mostrando loader');
     return <div>Loading...</div>;
